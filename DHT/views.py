50ea3458eebdcbc1715e95a8e043d53135a5c8f5
Boulehrouz Mohammed <bmdamv@gmail.com>
2024-01-06 03:38:51 +0100

    final project

--- DHT/views.py
+++ DHT/views.py
@@ -115,3 +115,8 @@ def sendtele():
     bot = telepot.Bot(token)
     bot.sendMessage(rece_id, 'la température depasse la normale')
     print(bot.sendMessage(rece_id, 'OK.'))
+
+class Dhtviews(generics.ListCreateAPIView):
+    queryset = Dht11.objects.all()
+    serializer_class = DHT11serialize
+
